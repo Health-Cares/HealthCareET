edaa9e00bae507abdfba0077ff3189769ce1219d
Reid Fekadu <redu8861989@gmail.com>
2019-07-11 16:08:22 -0700

    Data 2

--- app/src/main/java/com/example/healthcareet/data/Medical.kt
+++ app/src/main/java/com/example/healthcareet/data/Medical.kt
@@ -6,8 +6,10 @@ import androidx.room.PrimaryKey
 import java.io.Serializable
 
 @Entity(tableName = "medicals")
-data class Medical (@PrimaryKey @ColumnInfo(name = "first_name") val fname:String,
-                    @ColumnInfo(name = "last_name") val lname:String,
+data class Medical (@PrimaryKey @ColumnInfo(name = "first_name") val first:String,
+                    @ColumnInfo(name = "last_name") val last:String,
+                    @ColumnInfo(name = "age") val age: String,
+                    @ColumnInfo(name = "sex") val sex:String,
                     @ColumnInfo(name = "hospital") val hospital:String,
                     @ColumnInfo(name = "doctor") val doctor:String,
                     @ColumnInfo(name= "card") val card:Int,
