59f0c27cef39638c863e496065ca06c73abf0d8d
Reid Fekadu <redu8861989@gmail.com>
2019-07-11 18:30:51 -0700

    Add Medical Fragment

--- app/src/main/java/com/example/healthcareet/AddMedicalFragment.kt
+++ app/src/main/java/com/example/healthcareet/AddMedicalFragment.kt
@@ -1,12 +1,18 @@
 package com.example.healthcareet
 
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import com.example.healthcareet.data.Medical
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.fragment_add_medical.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,17 +24,36 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
+
 class AddMedicalFragment : Fragment() {
 
     private lateinit var saveButton: Button
+    private lateinit var first:TextInputEditText
+    private lateinit var last:EditText
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         return inflater.inflate(R.layout.fragment_add_medical, container, false)
-    }
 
+//        saveButton = save_button
+//        first =firstN
+//        last = lastN
+//
+//
+//
+//        saveButton.setOnClickListener {
+//
+//            val medical = readFeilds()
+//            val replyMedicalIntent = Intent()
+//            replyMedicalIntent.putExtra("medical_database",medical)
+//            setResult(Activity.RESULT_OK, replyMedicalIntent)
+//            finish()
+//
+//        }
+    }
 
 
 }
