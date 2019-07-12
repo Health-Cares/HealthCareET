0bfa0aba9ef3f6c9d5fccac294b51aa5636cfef7
Reid Fekadu <redu8861989@gmail.com>
2019-07-11 18:28:25 -0700

    Repository 2

--- app/src/main/java/com/example/healthcareet/repository/MedicalRepository.kt
+++ app/src/main/java/com/example/healthcareet/repository/MedicalRepository.kt
@@ -10,7 +10,13 @@ class MedicalRepository(private val medicalDao: MedicalDao) {
     fun allMedical(): LiveData<List<Medical>> = medicalDao.getAllMedicals()
 
     fun insertMedical(medical: Medical) {
-        medicalDao.insertMedicals(medical)
+        medicalDao.insertMedical(medical)
+    }
+    fun updateMedical(medical: Medical){
+        medicalDao.updateMedical(medical)
+    }
+    fun deleteMedical(medical: Medical){
+        medicalDao.deleteMedical(medical)
     }
 }
 
